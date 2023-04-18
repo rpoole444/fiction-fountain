e5256586e23c13cc99c3c00e5676994be5afbbd5
Reid Poole <poole.reid@gmail.com>
2023-04-18 09:03:09 -0500

    add Image Generator and extra Styling

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <section className="intro-container">
+      <h1>Welcome to Fiction Fountain</h1>
+      <p>
+        Welcome to Fiction Fountain where with a simple prompt you are able to
+        create a custimized story!
+      </p>
+    </section>
+  );
+};
+
+export default Header;
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import CanvasImage from "../assets/CanvasImage.png";
+import CoolBook from "../assets/CoolBook.png";
 import "./HomePage.css";
+import Header from "../Header/Header";
 
 const HomePage: React.FC = () => {
   const history = useHistory();
@@ -15,14 +18,8 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <>
-      <section className="intro-container">
-        <h1>Welcome to Fiction Fountain</h1>
-        <p>
-          Welcome to Fiction Fountain where with a simple prompt you are able to
-          create a custimized story!
-        </p>
-      </section>
+    <div className="full-homepage">
+      <Header />
       <div className="HomePage">
         <section className="story-generator-container">
           <section>
@@ -31,26 +28,28 @@ const HomePage: React.FC = () => {
               Make a story for any occasion, but perfect for bedtime and
               storytime!
             </p>
+            <button onClick={navigateToCustomPrompts}>
+              Create your custom prompt
+            </button>
           </section>
-          <button onClick={navigateToCustomPrompts}>
-            Create your custom prompt
-          </button>
+          <img src={CoolBook} />
         </section>
         <section className="image-generator-container">
+          <img src={CanvasImage} />
           <section>
             <h2>Image Generator</h2>
+            <p>In addition to our Story Generator, Try our image generator!</p>
             <p>
-              In addition to our Story Generator, Try our image generator! Give
-              our ai a prompt and see what fictional image is created for your
-              enjoyment
+              Give Our ai a prompt and see what fictional image is created for
+              your enjoyment!
             </p>
+            <button onClick={navigateToCustomPrompts}>
+              Create your custom prompt
+            </button>
           </section>
-          <button onClick={navigateToCustomPrompts}>
-            Create your custom prompt
-          </button>
         </section>
       </div>
-    </>
+    </div>
   );
 };
 
