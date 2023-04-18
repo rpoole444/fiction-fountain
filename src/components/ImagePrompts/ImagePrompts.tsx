6d590c575db6e802c48b62b0867c0777d8b820b4
Reid Poole <poole.reid@gmail.com>
2023-04-18 09:22:36 -0500

    Add Image Prompt page

---
+++ src/components/ImagePrompts/ImagePrompts.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./ImagePrompts.css";
+
+const ImagePrompts: React.FC = () => {
+  const [imagePrompt, setImagePrompt] = useState<string>("");
+  const history = useHistory();
+
+  const handleImageInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setImagePrompt(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!imagePrompt.trim()) {
+      history.push({
+        pathname: "/image",
+        state: { prompt: imagePrompt },
+      });
+    }
+  };
+
+  return (
+    <div className="CustomImagePrompts">
+      <h1>Create Your Custom Image Prompt</h1>
+      <input
+        type="text"
+        value={imagePrompt}
+        onChange={(e) => {
+          handleImageInput(e);
+        }}
+        placeholder="Enter your custom image prompt here"
+      />
+      <button onClick={handleSubmit}>Generate Image</button>
+    </div>
+  );
+};
+
+export default ImagePrompts;
