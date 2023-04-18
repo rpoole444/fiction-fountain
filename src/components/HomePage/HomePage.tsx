2c9d74185bfa76283618aa1c343b79fc398c7733
Reid Poole <poole.reid@gmail.com>
2023-04-18 08:26:43 -0500

    Add Minor Stylings

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -15,12 +15,42 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <div className="HomePage">
-      <h1>Welcome to FictionFountain</h1>
-      <button onClick={navigateToCustomPrompts}>
-        Create your custom prompt
-      </button>
-    </div>
+    <>
+      <section className="intro-container">
+        <h1>Welcome to Fiction Fountain</h1>
+        <p>
+          Welcome to Fiction Fountain where with a simple prompt you are able to
+          create a custimized story!
+        </p>
+      </section>
+      <div className="HomePage">
+        <section className="story-generator-container">
+          <section>
+            <h2> Story Generator</h2>
+            <p>
+              Make a story for any occasion, but perfect for bedtime and
+              storytime!
+            </p>
+          </section>
+          <button onClick={navigateToCustomPrompts}>
+            Create your custom prompt
+          </button>
+        </section>
+        <section className="image-generator-container">
+          <section>
+            <h2>Image Generator</h2>
+            <p>
+              In addition to our Story Generator, Try our image generator! Give
+              our ai a prompt and see what fictional image is created for your
+              enjoyment
+            </p>
+          </section>
+          <button onClick={navigateToCustomPrompts}>
+            Create your custom prompt
+          </button>
+        </section>
+      </div>
+    </>
   );
 };
 
